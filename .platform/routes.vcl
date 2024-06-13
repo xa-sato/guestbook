918a8e95d88d1a7f963d0b6c854866443a44f7e0
xa-sato <y.sato@xearts.jp>
2024-06-13 16:20:10 +0900

    section21 done

---
+++ .platform/routes.vcl
@@ -0,0 +1,2 @@
+"https://{all}/": { type: upstream, upstream: "varnish:http", cache: { enabled: false } }
+"http://{all}/": { type: redirect, to: "https://{all}/" }
---
+++ src/Controller/AdminController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Message\CommentMessage;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpCache\StoreInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Workflow\WorkflowInterface;
+use Twig\Environment;
+
+#[Route('/admin')]
+class AdminController extends AbstractController
+{
+    public function __construct(
+        private Environment $twig,
+        private EntityManagerInterface $entityManager,
+        private MessageBusInterface $bus,
+    ) {
+    }
+
+    #[Route('/comment/review/{id}', name: 'review_comment')]
+    public function reviewComment(Request $request, Comment $comment, WorkflowInterface $commentStateMachine): Response
+    {
+        $accepted = !$request->query->get('reject');
+
+        if ($commentStateMachine->can($comment, 'publish')) {
+            $transition = $accepted ? 'publish' : 'reject';
+        } elseif ($commentStateMachine->can($comment, 'publish_ham')) {
+            $transition = $accepted ? 'publish_ham' : 'reject_ham';
+        } else {
+            return new Response('Comment already reviewed or not in the right state.');
+        }
+
+        $commentStateMachine->apply($comment, $transition);
+        $this->entityManager->flush();
+
+        if ($accepted) {
+            $this->bus->dispatch(new CommentMessage($comment->getId()));
+        }
+
+        return new Response($this->twig->render('admin/review.html.twig', [
+            'transition' => $transition,
+            'comment' => $comment,
+        ]));
+    }
+    #[Route('/http-cache/{uri<.*>}', methods: ['PURGE'])]
+    public function purgeHttpCache(KernelInterface $kernel, Request $request, string $uri, StoreInterface $store): Response
+    {
+        if ('prod' === $kernel->getEnvironment()) {
+            return new Response('KO', 400);
+        }
+
+        $store->purge($request->getSchemeAndHttpHost().'/'.$uri);
+
+        return new Response('Done');
+    }
+}

16d321e2f20884cb2cc6a7aa49c4327358771591
xa-sato <y.sato@xearts.jp>
2024-06-20 16:58:30 +0900

    section28 'URLを国際化する'

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -26,7 +26,13 @@ class ConferenceController extends AbstractController
     ) {
     }
 
-    #[Route('/', name: 'homepage')]
+    #[Route('/')]
+    public function indexNoLocale(): Response
+    {
+        return $this->redirectToRoute('homepage', ['_locale' => 'en']);
+    }
+
+    #[Route('/{_locale<%app.supported_locales%>}/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {
         return $this->render('conference/index.html.twig', [
@@ -42,7 +48,7 @@ class ConferenceController extends AbstractController
         ])->setSharedMaxAge(3600);
     }
 
-    #[Route('/conference/{slug}', name: 'conference')]
+    #[Route('/{_locale<%app.supported_locales%>}/conference/{slug}', name: 'conference')]
     public function show(
         Request $request,
         Conference $conference,

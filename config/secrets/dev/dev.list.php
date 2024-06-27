9f2c7f1a60391fa87d563204aeda0a2c0c579a93
xa-sato <y.sato@xearts.jp>
2024-06-27 16:47:46 +0900

    section29 done

--- config/secrets/dev/dev.list.php
+++ config/secrets/dev/dev.list.php
@@ -2,4 +2,6 @@
 
 return [
     'AKISMET_KEY' => null,
+    'BLACKFIRE_SERVER_ID' => null,
+    'BLACKFIRE_SERVER_TOKEN' => null,
 ];
---
+++ config/secrets/prod/prod.list.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'BLACKFIRE_SERVER_ID' => null,
+    'BLACKFIRE_SERVER_TOKEN' => null,
+];
